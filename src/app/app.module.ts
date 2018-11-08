c6528a6fe24e44f30f806df89ad532b47031dded
Ron Almog <ron8132@gmail.com>
2018-11-07 23:43:04 -0500

    home page, basic structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TopbarComponent } from './components/topbar/topbar.component';
+import { TransferComponent } from './components/transfer/transfer.component';
+import { TransactionsComponent } from './components/transactions/transactions.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TopbarComponent
+    TopbarComponent,
+    TransferComponent,
+    TransactionsComponent,
+    HomeComponent
 
   ],
   imports: [
