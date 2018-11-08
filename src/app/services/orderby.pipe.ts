9ab70b62d1ab09fee7416bb20210b5940983a4f5
Ron Almog <ron8132@gmail.com>
2018-11-08 15:36:44 -0500

    fix bug: compare numbers

--- src/app/services/orderby.pipe.ts
+++ src/app/services/orderby.pipe.ts
@@ -11,35 +11,22 @@ export class OrderByPipe implements PipeTransform {
 
   private sort(value: any[], key?: any, reverse?: boolean): any[] {
     const isInside = key && key.indexOf('.') !== -1;
-    if (isInside) {
-      key = key.split('.');
-    }
 
     const array: any[] = value.sort((a: any, b: any): number => {
       if (!key) {
         return a > b ? 1 : -1;
       }
 
-      if (!isInside) {
+      if (key === 'amount') {
+        return a[key] - b[key]; // compare numbers ;)
+      } else {
         return a[key] > b[key] ? 1 : -1;
       }
-      return this.getValue(a, key) > this.getValue(b, key) ? 1 : -1;
     });
 
     if (reverse) {
       return array.reverse();
     }
     return array;
-  }
-
-  private getValue(object: any, key: string[]) {
-    for (let i = 0, n = key.length; i < n; ++i) {
-      const k = key[i];
-      if (!(k in object)) {
-        return;
-      }
-      object = object[k];
-    }
-    return object;
   }
 }
