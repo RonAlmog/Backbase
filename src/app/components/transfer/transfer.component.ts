073e8b8ed25cb64c2381524aa32dfdd73188e1f5
Ron Almog <ron8132@gmail.com>
2018-11-10 10:59:52 -0500

    transaction preview, form validation

--- src/app/components/transfer/transfer.component.ts
+++ src/app/components/transfer/transfer.component.ts
@@ -11,6 +11,7 @@ export class TransferComponent implements OnInit {
   @Output() pushTransaction = new EventEmitter();
 
   tranForm: FormGroup;
+  submitted = false;
   demoTransaction: any = {
     'amount': '99.99',
     'categoryCode': '#ff0000',
@@ -19,21 +20,59 @@ export class TransferComponent implements OnInit {
     'transactionDate': 1476836642000,
     'transactionType': 'Online Transfer'
   };
+  accountName = 'Free Checking(4692)';
+  balance = 1000.76;
+  amount = 0;
+  toAccount = '';
+  showPreview = false;
+
   constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit() {
     this.tranForm = this.formBuilder.group({
-      fromAccount: new FormControl('Free Checking(4692) - $5824.76', [Validators.required]),
+      // fromAccount: new FormControl('', [Validators.required]),
       toAccount: new FormControl('', [Validators.required]),
-      amount: new FormControl('0', [Validators.required])
+      amount: new FormControl('', [Validators.required])
     });
   }
 
-  makeTransfer() {
+  // convenience getter ;)
+  get f() { return this.tranForm.controls; }
+
+  preview() {
+    this.submitted = true;
+    if (this.tranForm.invalid) {
+      return;
+    }
+    console.log('form', this.tranForm.value);
+    console.log('amount', this.tranForm.value.amount);
+    this.amount = this.asNumber(this.tranForm.value.amount);
+    this.toAccount = this.tranForm.value.toAccount;
+    this.showPreview = true;
+  }
+
+  transfer() {
     console.log('form', this.tranForm.value);
     this.demoTransaction.merchantLogo = this.transactions[0].merchantLogo,
     this.pushTransaction.emit(this.demoTransaction);
 
+  }
+
+  validate(): boolean {
+    console.log('amount', this.tranForm.value.amount);
+    return true;
+
+  }
+
+  asNumber(num) {
+    if (Number.isNaN(Number.parseFloat(num))) {
+      return 0;
+    }
+    return parseFloat(num);
+  }
+
+  cancelPreview() {
+    this.showPreview = false;
   }
 
 }
